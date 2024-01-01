0435eefc6e8cefc9a09d300a9f8a1eadba208dd5
Vaclav Matejka <matejka.vaclav@gmail.com>
2024-01-01 23:12:57 +0100

    init: project structure sketch

---
+++ model/gaforecast.py
@@ -0,0 +1,15 @@
+from sklearn.base import BaseEstimator
+
+class GAForecast(BaseEstimator):
+    def __init__(self):
+        # Load your model here
+
+    def fit(self, X, y=None):
+        # Method for fitting the model (if your model is pre-trained, you might not need to change anything here)
+        return self
+
+    def predict(self, X):
+        # Method for making predictions
+        return predictions
+
+    # Implement other methods as needed
